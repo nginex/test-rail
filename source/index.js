a6c55cf53d34e3225e90038471ae706e4b2c352f
nginex <o.kuzava@internetdevels.com>
2016-06-12 20:58:12 +0300

    Added example file with results.

--- source/index.js
+++ source/index.js
@@ -1,11 +1,34 @@
 #!/usr/bin/env node
 "use strict";
 
+// Check if arguments exist.
+const args = process.argv.slice(2);
+if (!args.length) {
+  throw new Error('There are no arguments. See help.');
+}
+
+// Import configs and modules.
 import TestRail from 'node-testrail';
+import fs from 'fs';
 import config from '../config.json';
+
+fs.access(args[0], fs.R_OK, (err) => {
+  if (err) {
+    throw err;
+  }
+});
+
+const results = require(args[0]);
 
 const url = config.account.url;
 const email = config.account.email;
 const password = config.account.password;
 
 const TR = new TestRail(url, email, password);
+
+// Check if account data in TestRail is correct.
+TR.getUserByEmail(email, (user) => {
+  if (typeof user === 'undefined') {
+    throw new Error('There is no account in TestRail. See help to set correct user data.');
+  }
+});
