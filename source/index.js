f5d8e67ce6ec2b1c852d80bb14ba4cc5d4d257cb
nginex <o.kuzava@internetdevels.com>
2016-06-12 17:42:25 +0300

    Created basic implementation of npm module.

---
+++ source/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import TestRail from 'node-testrail';
+import config from '../config.json';
+
+const url = config.account.url;
+const email = config.account.email;
+const password = config.account.password;
+
+const TR = new TestRail(url, email, password);
+
