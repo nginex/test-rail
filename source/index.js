21e5789090bc3b02ce480f29b2264a05dc51f15e
nginex <o.kuzava@internetdevels.com>
2016-06-15 12:11:18 +0300

    Extended helper class to manage TestRail components.

--- source/index.js
+++ source/index.js
@@ -55,23 +55,102 @@ if (typeof results.tests === 'undefined' || !results.tests.length) {
   throw new Error('There are no test cases.');
 }
 
+/**
+ * Class as helper for node-testrail functionality.
+ */
 class TestRailHelper {
-  constructor(name) {
-    this.projectName = name;
-  }
 
-  get projectId() {
+  /**
+   * Get id of project by name.
+   * @param name
+   * @param tests
+   */
+  constructor(name, tests) {
+    this.projectName = name;
+    this.tests = tests;
     TR.getProjects((d) => {
       const projects = JSON.parse(d);
       for (let i = 0; i < projects.length; i++) {
         if (projects[i].name == this.projectName) {
-          return projects[i].id;
+          this.projectId = projects[i].id;
+          break;
         }
       }
-      throw new Error('There are no project with entered name.');
+
+      if (typeof this.projectId === 'undefined') {
+        throw new Error('There are no project with entered name.');
+      }
+
+      let amountTests = this.tests.length;
+      for (let i = 0; i < amountTests; i++) {
+        this.test = this.tests[i];
+        this.manageSuites();
+      }
+    });
+  }
+
+  /**
+   * Get id of suite by name or create new one.
+   */
+  manageSuites() {
+    this.suiteName = this.test.suite || 'Master';
+    TR.getSuites(this.projectId, (d) => {
+      const suites = JSON.parse(d);
+      for (let i = 0; i < suites.length; i++) {
+        if (suites[i].name == suiteName) {
+          this.suiteId = suites[i].id;
+          break;
+        }
+      }
+
+      if (typeof this.suiteId === 'undefined') {
+        TR.addSuite(this.projectId, this.suiteName, null, (d) => {
+          let suite = JSON.parse(d);
+          this.suiteId = suite.id;
+          this.manageSections();
+        });
+      }
+      else {
+        this.manageSections();
+      }
+    });
+  }
+
+  /**
+   * Get id of section by name or create new one.
+   */
+  manageSections() {
+    this.sectionName = this.test.section || 'Test Cases';
+    TR.getSections(this.projectId, this.suiteId, (d) => {
+      const sections = JSON.parse(d);
+      for (let i = 0; i < sections.length; i++) {
+        if (sections[i].name == this.sectionName) {
+          this.sectionId = sections[i].id;
+          break;
+        }
+      }
+
+      if (typeof this.sectionId === 'undefined') {
+        TR.addSection(this.projectId, this.suiteId, null, this.sectionName, (d) => {
+          let section = JSON.parse(d);
+          this.sectionId = section.id;
+          this.manageTestCases();
+        });
+      }
+      else {
+        this.manageTestCases();
+      }
+    });
+  }
+
+  /**
+   * Get id of test case by name or create new one.
+   */
+  manageTestCases() {
+    TR.getCases(this.projectId, this.suiteId, this.sectionId, (d) => {
+      const cases = JSON.parse(d);
     });
   }
 }
 
-let testRail = new TestRailHelper(project);
-const amountTestCases = results.tests.length;
+new TestRailHelper(project, results.tests);
